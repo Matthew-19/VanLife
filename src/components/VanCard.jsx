ab13427d51b63a089574241a0ce964095e93ab99
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2023-10-20 13:57:48 +0300

    added /VanLife in VanCard.jsx

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function VanCard(props) {
   return (
     <div className="van--card">
-      <Link to={`/vans/${props.id}`}>
+      <Link to={`/VanLife/vans/${props.id}`}>
         <img className="van--img" src={props.img} alt="van-image" />
         <div className="van--info">
           <div className="van--name">{props.name}</div>
